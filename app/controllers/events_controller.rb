4404fc995aab4b567d57fc5d975a7e005778f277
Werner <turbotimoh@gmail.com>
2023-04-12 12:42:20 +0545

    Add `authenticate_user` before action to `new` and `create` methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,14 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
   end
 end
