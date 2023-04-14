0c0e5d921ee663ecefc2dc6a1f74983b042de162
Werner <turbotimoh@gmail.com>
2023-04-14 09:17:41 +0545

    Bug Fix: Display routed user events instead of current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @user = current_user
-    @events = current_user.events
+    @user = User.find(params[:id])
+    @events = @user.events
   end
 end
