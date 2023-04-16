32431dc408e8db99e7b84d1a32d8c3b8522f563d
Werner <turbotimoh@gmail.com>
2023-04-16 08:50:26 +0545

    Allow only creators to add attendees to event (semi private events)

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,13 +1,24 @@
 class InvitationsController < ApplicationController
   def new
     @invitation = Invitation.new
+    @events = current_user.events
   end
 
   def create
     event_id = params[:invitation][:event_id]
+    
+    unless event_id
+      flash[:alert] = 'Invalid event'
+      redirect_to events_path and return
+    end
 
     if Invitation.where(invite_params).any?
       flash[:alert] = 'Already attending this event'
+      redirect_to event_path(event_id) and return
+    end
+
+    unless current_user.events.include? Event.find(event_id)
+      flash[:alert] = 'Only creators can invite to event'
       redirect_to event_path(event_id) and return
     end
 
